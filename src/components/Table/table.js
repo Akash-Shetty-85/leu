0ce0db7b3df9c7689ea3c7d43b6f5fca6d12b2a1
Akash V <aakashvshetty@gmail.com>
2023-11-26 10:00:07 +0530

    updates

--- src/components/Table/table.js
+++ src/components/Table/table.js
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react';
+  import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Table = () => {
   const [userData, setUserData] = useState([]);
+  const [editingUserId, setEditingUserId] = useState(null);
+  const [editedData, setEditedData] = useState({});
 
   useEffect(() => {
     fetchData();
-  }, []); // The empty dependency array ensures the effect runs only once on mount
+  }, []);
 
   const fetchData = () => {
     axios.get("http://localhost:3001/users")
       .then((response) => {
-        console.log(response.data);
         setUserData(response.data);
       })
       .catch((error) => {
@@ -22,17 +23,37 @@ const Table = () => {
   const handleDelete = (id) => {
     axios.delete(`http://localhost:3001/users/${id}`)
       .then(() => {
-        fetchData(); // Fetch updated data after deletion
+        fetchData();
       })
       .catch((error) => {
         console.error("Error deleting user:", error);
       });
   };
 
-  // Listen for changes in userData (triggered by form submission)
-//   useEffect(() => {
-//     fetchData();
-//   }, [userData]);
+  const handleEdit = (id, data) => {
+    setEditingUserId(id);
+    setEditedData(data);
+  };
+
+  const handleUpdate = (id) => {
+    // Send a PUT request to update the user
+    axios.put(`http://localhost:3001/users/${id}`, editedData)
+      .then(() => {
+        setEditingUserId(null);
+        setEditedData({});
+        fetchData(); // Fetch updated data after the update
+      })
+      .catch((error) => {
+        console.error("Error updating user:", error);
+      });
+  };
+
+  const handleInputChange = (fieldName, value) => {
+    setEditedData((prevData) => ({
+      ...prevData,
+      [fieldName]: value,
+    }));
+  };
 
   return (
     <div>
@@ -44,20 +65,55 @@ const Table = () => {
             <th>email</th>
             <th>phone</th>
             <th>ID</th>
-            <th>delete</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
           {userData.map((data) => (
             <tr key={data._id}>
-              <td>{data.name}</td>
-              <td>{data.email}</td>
-              <td>{data.phone}</td>
+              <td>
+                {editingUserId === data._id ? (
+                  <input
+                    type="text"
+                    value={editedData.name || data.name}
+                    onChange={(e) => handleInputChange("name", e.target.value)}
+                  />
+                ) : (
+                  data.name
+                )}
+              </td>
+              <td>
+                {editingUserId === data._id ? (
+                  <input
+                    type="text"
+                    value={editedData.email || data.email}
+                    onChange={(e) => handleInputChange("email", e.target.value)}
+                  />
+                ) : (
+                  data.email
+                )}
+              </td>
+              <td>
+                {editingUserId === data._id ? (
+                  <input
+                    type="text"
+                    value={editedData.phone || data.phone}
+                    onChange={(e) => handleInputChange("phone", e.target.value)}
+                  />
+                ) : (
+                  data.phone
+                )}
+              </td>
               <td>{data._id}</td>
               <td>
-                <button onClick={() => handleDelete(data._id)}>
-                  Delete
-                </button>
+                {editingUserId === data._id ? (
+                  <button onClick={() => handleUpdate(data._id)}>Update</button>
+                ) : (
+                  <>
+                    <button onClick={() => handleEdit(data._id, data)}>Edit</button>
+                    <button onClick={() => handleDelete(data._id)}>Delete</button>
+                  </>
+                )}
               </td>
             </tr>
           ))}
